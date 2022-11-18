a558b40ccd74f72e2bd815c306459cf4268e3f84
jm server2 <jm@mazoea.com>
2022-11-18 23:46:22 +0100

    [devOps] moving to github

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,6 @@
-git push origin :u16g8py38
-git tag -d u16g8py38
-git tag u16g8py38
-git push origin te_base_u16_g8_py38 --tags
+git push origin :u18g8py38
+git tag -d u18g8py38
+git tag u18g8py38
+git push origin te_base_u18_g8_py38 --tags
 
 pause
