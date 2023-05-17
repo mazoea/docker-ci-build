2d0b7c36039ac57028349cc82f04ef52b50fe8c8
jm server2 <jm@mazoea.com>
2023-05-17 09:58:55 +0200

    added te-ml deps

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,6 @@
-git push origin :u18g8py38
-git tag -d u18g8py38
-git tag u18g8py38
-git push origin te_base_u18_g8_py38 --tags
+git push origin :u18g8py38_full
+git tag -d u18g8py38_full
+git tag u18g8py38_full
+git push origin te_base_u18_g8_py38_full --tags
 
 pause
