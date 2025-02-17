9c0f8757088442a9f0bdd3b77b73f1434154adc3
TooMindoo <tomas.minac@dataquest.sk>
2025-02-17 13:58:53 +0100

    Version python to 3.9

--- scripts/update.tag.bat
+++ scripts/update.tag.bat
@@ -1,6 +1,6 @@
-git push origin :u20g8py38full
-git tag -d u20g8py38full
-git tag u20g8py38full
-git push origin te_base_u20_g8_py38_full --tags
+git push origin :u20g8py39full
+git tag -d u20g8py39full
+git tag u20g8py39full
+git push origin te_base_u20_g8_py39_full --tags
 
 pause
